b0d4c1bb6a09b302d8fb5351963bd877fdf5b26f
Allan Pearson <Alprsn2012@gmail.com>
2019-07-23 04:25:25 -0500

    line 1 added "button").om(click) line 3 spacing line 4 my code for my API was not working/ borrowed code from in class project. line 8-10 found coding referance for ajax at w3schools. line 14 had to console log errors made my code not work. line 16 console log response for line 13 due to syntax error spelled wrong. lines 20 - 32 i used a var music div to get my rating, results, with a fixed image height url.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,15 +1,35 @@
-document.getElementById("button1").addEventListener("click", function () {
-    document.getElementById("box").style.height = "250px";
-});
+$("button").on("click", function() {
+    var music = $(this).attr("data-music");
 
-document.getElementById("button2").addEventListener("click", function () {
-    document.getElementById("box").style.backgroundColor = "Blue";
-});
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+music + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
-document.getElementById("button3").addEventListener("click", function () {
-    document.getElementById("box").style.opacity = .5;
-});
 
-document.getElementById("button4").addEventListener("click", function () {
-    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px; opacity=1;";
+$.ajax({
+url: queryURL,
+method: "GET"
+})
+
+.then(function(response) {
+  console.log(queryURL);
+
+  console.log(response);
+  
+  var results = response.data;
+  
+  for (var i = 0; i < results.length; i++) {
+
+                var musicDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var musicImage = $("<img>");
+                musicImage.attr("src", results[i].images.fixed_height.url);
+
+    
+    musicDiv.append(p);
+    musicDiv.append(musicImage);
+
+   
+    $("#gifs-appear-here").prepend(musicDiv);
+  }
+});
 });
